4324684b5610de5a76909e70b977aa8532048e42
Julian Rincon <julian1918@hotmail.es>
2023-06-11 18:38:39 -0500

    settings

--- src/app/components/documents/client-documents/client-documents.component.ts
+++ src/app/components/documents/client-documents/client-documents.component.ts
@@ -1,44 +1,21 @@
 import { Component } from '@angular/core';
-import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-client-documents',
   templateUrl: './client-documents.component.html',
-  styleUrls: ['./client-documents.component.scss']
+  styleUrls: ['./client-documents.component.scss'],
 })
 export class ClientDocumentsComponent {
-
-  closeResult = '';
   documentCurrent: any = {};
 
-  constructor(private modalService: NgbModal) { }
-
-  open(content: any, document: any) {
+  open(document: any) {
     this.documentCurrent = document;
-    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
-      (result) => {
-        this.closeResult = `Closed with: ${result}`;
-      },
-      (reason) => {
-        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-      },
-    );
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
   }
 
   documents = [
-    { id: 1, name: 'Documento 1', status: 'Aprobado' },
-    { id: 2, name: 'Documento 2', status: 'Pendiente' },
-    { id: 3, name: 'Documento 3', status: 'Rechazado' },
+    { id: 1, name: 'Contrato 1', status: 'Aprobado' },
+    { id: 2, name: 'Contrato 2', status: 'Pendiente' },
+    { id: 3, name: 'Contrato 3', status: 'Rechazado' },
     // ...
   ];
 
@@ -51,6 +28,4 @@ export class ClientDocumentsComponent {
     // Lógica para rechazar el documento con la ID especificada
     console.log(`Rechazar documento con ID ${id}`);
   }
-
-
 }
--- src/app/components/documents/reviewer-documents/reviewer-documents.component.ts
+++ src/app/components/documents/reviewer-documents/reviewer-documents.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./reviewer-documents.component.scss']
 })
 export class ReviewerDocumentsComponent {
+  documentCurrent: any = {};
 
+  open(document: any) {
+    this.documentCurrent = document;
+  }
+
+  documents = [
+    { id: 1, name: 'Contrato 1', status: 'Aprobado' },
+    { id: 2, name: 'Contrato 2', status: 'Pendiente' },
+    { id: 3, name: 'Contrato 3', status: 'Rechazado' },
+    // ...
+  ];
+
+  verDocumento(id: number) {
+    // Lógica para ver el documento con la ID especificada
+    console.log(`Ver documento con ID ${id}`);
+  }
+
+  rechazarDocumento(id: number) {
+    // Lógica para rechazar el documento con la ID especificada
+    console.log(`Rechazar documento con ID ${id}`);
+  }
 }
